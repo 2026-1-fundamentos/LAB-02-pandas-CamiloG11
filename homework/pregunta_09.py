58f7f5e3d2cfd160ae2de0b6884f9bab5d66801f
camilo1392 <cgutierrezgon@unal.edu.co>
2026-07-01 11:48:34 -0500

    intento 1

--- homework/pregunta_09.py
+++ homework/pregunta_09.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_09():
     """
@@ -23,3 +24,8 @@ def pregunta_09():
     39  39  E   5  1998-01-26  1998
 
     """
+    df = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    df['c3'] = pd.to_datetime(df['c3'])
+    df['year'] = df['c3'].dt.year.astype(str)
+    return df 
+print(pregunta_09())
